cb58c36dab28d971d1ba2094c46a517a28aba051
Ismat Jerin Arpa <ismatjerinarpa@gmail.com>
2024-05-17 05:27:57 +0600

    explore routing in express

--- dist/app/app.js
+++ dist/app/app.js
@@ -9,6 +9,28 @@ const port = 3000;
 // Parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Course is created successfully",
+        data: course,
+    });
+});
 // Middleware
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
@@ -18,15 +40,19 @@ app.get("/", logger, (req, res) => {
     res.send("Hello!");
 });
 // Use of params
-app.get("/:userId/:subId", logger, (req, res) => {
-    console.log(req.params);
-    res.send("Hello!");
+/*
+app.get("/:userId/:subId", logger, (req: Request, res: Response) => {
+  console.log(req.params);
+  res.send("Hello!");
 });
+*/
 // Use of query
-app.get("/", logger, (req, res) => {
-    console.log(req.query);
-    res.send("Find data successfully");
+/*
+app.get("/", logger, (req: Request, res: Response) => {
+  console.log(req.query);
+  res.send("Find data successfully");
 });
+*/
 app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.send("Got the data");
--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,34 @@ const port = 3000;
 app.use(express.json());
 app.use(express.text());
 
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
+});
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+
+  res.json({
+    success: true,
+    message: "Course is created successfully",
+    data: course,
+  });
+});
+
 // Middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
@@ -18,16 +46,20 @@ app.get("/", logger, (req: Request, res: Response) => {
 });
 
 // Use of params
+/* 
 app.get("/:userId/:subId", logger, (req: Request, res: Response) => {
   console.log(req.params);
   res.send("Hello!");
-});
+}); 
+*/
 
 // Use of query
+/*
 app.get("/", logger, (req: Request, res: Response) => {
   console.log(req.query);
   res.send("Find data successfully");
-});
+}); 
+*/
 
 app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
